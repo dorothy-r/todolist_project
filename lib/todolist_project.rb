3d30dfbaf3bd434f1c96199cdb93c3a7eaf52852
Dorothy Rhodes <70044671+dorothy-r@users.noreply.github.com>
2022-07-08 14:43:45 -0400

    Gemfile updates

--- lib/todolist_project.rb
+++ lib/todolist_project.rb
@@ -1,7 +1,7 @@
+require 'bundler/setup'
 # This class represents a todo item and its associated
 # data: name and description. There's also a "done"
 # flag to show whether this todo item is done.
-
 class Todo
   DONE_MARKER = 'X'
   UNDONE_MARKER = ' '
